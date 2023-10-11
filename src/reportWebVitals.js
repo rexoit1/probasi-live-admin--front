6ecaef243fdc434f6570822cd7e57f23edbd78e6
rexoit1 <developer.rexoit@gmail.com>
2023-10-11 12:10:41 +0600

    baseurl changed

--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,6 +1,6 @@
 const reportWebVitals = onPerfEntry => {
   if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP,            }) => {
+    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP,getTTFB}) => {
       getCLS(onPerfEntry);
       getFID(onPerfEntry);
       getFCP(onPerfEntry);
@@ -11,3 +11,4 @@ const reportWebVitals = onPerfEntry => {
 };
 
 export default reportWebVitals;
+
